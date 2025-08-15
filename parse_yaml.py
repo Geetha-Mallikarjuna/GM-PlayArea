28373e5b01489f1f5df10054c7f9d1cc41a449e1
Geetha-Mallikarjuna <geetha.mallikarjuna@cbng.co.uk>
2025-08-15 14:20:39 +0100

    Update parse_yaml.py

--- parse_yaml.py
+++ parse_yaml.py
@@ -1,22 +1,26 @@
 import yaml
 import os
 
-# Load YAML
 with open('input.yaml', 'r') as f:
     data = yaml.safe_load(f)
 
 require_files = data.get('require_files', [])
 
-# Ensure output directory
 os.makedirs('changelogs', exist_ok=True)
 
-# Write markdown table
 with open('changelogs/packages-info.txt', 'w') as out:
-    out.write('# 📦 Package Summary from input.yaml\n')
-    out.write('| Package | Version | Name |\n')
-    out.write('|---------|---------|------|\n')
+    out.write('# 📦 Package Summary from input.yaml\n\n')
+    # Write HTML table start tag
+    out.write('<table border="1" cellpadding="5" cellspacing="0">\n')
+    out.write('  <thead>\n')
+    out.write('    <tr><th>Package</th><th>Version</th><th>Name</th></tr>\n')
+    out.write('  </thead>\n')
+    out.write('  <tbody>\n')
     for item in require_files:
         package = item.get('package', 'N/A')
         version = item.get('version', 'N/A')
         name = item.get('name', 'N/A')
-        out.write(f'| {package} | {version} | {name} |\n')
+        # Write each row as HTML
+        out.write(f'    <tr><td>{package}</td><td>{version}</td><td>{name}</td></tr>\n')
+    out.write('  </tbody>\n')
+    out.write('</table>\n')
